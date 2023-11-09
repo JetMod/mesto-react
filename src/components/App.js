262abf33a82318b7f188c63c0fe34a90b9a92fae
JetMod <enver027@yandex.ru>
2023-11-10 01:05:51 +0300

    второе ревью

--- src/components/App.js
+++ src/components/App.js
@@ -28,8 +28,8 @@ function App() {
     setIsAddPlacePopupOpen(true);
   }
 
-  function handleCardClick(cardData) {
-    setSelectedCard(cardData);
+  function handleCardClick(card) {
+    setSelectedCard(card);
   }
 
   function closeAllPopups() {
